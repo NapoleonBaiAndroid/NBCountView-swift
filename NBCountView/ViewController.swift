f834436d7ecbbaf755e23e885521265cc300d272
NapoleonBai <napoleonbaiandroid@gmail.com>
2015-09-20 15:39:02 +0800

    Initial Commit

---
+++ NBCountView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NBCountView
+//
+//  Created by NapoleonBai on 15/9/20.
+//  Copyright © 2015年 NapoleonBai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

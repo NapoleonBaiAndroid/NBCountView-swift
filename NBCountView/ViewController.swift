81b9953b13cf938f84477794f32c5d7f24e0ef0e
NapoleonBai <napoleonbaiandroid@gmail.com>
2015-11-16 23:10:29 +0800

    提交swift版NBCountView
    提交swift版NBCountView

--- NBCountView/ViewController.swift
+++ NBCountView/ViewController.swift
@@ -9,10 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var countView : NBCountView = NBCountView.init(frame: CGRectMake(50, 100, 200, 50))
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        countView.isEditTextField = true
+        self.view.addSubview(countView)
+        countView.viewConfig = NBCountConfig(countViewShowType:.FilletButton,tintColor:UIColor.redColor())
+        countView.currentValue = 20
+        countView.notifyUpdateStepValue = {(updateStepValue : Int) in
+            print("更改的数量为:\(updateStepValue)")
+        }
+        countView.notifyUpdateCurrentValue = {(currentValue : Int) in
+            print("当前显示数值为:\(currentValue)")
+        }
+        
+        let countView1 : NBCountView = NBCountView.init(frame: CGRectMake(50, 160, 120, 40))
+        self.view.addSubview(countView1)
+        
+        countView1.viewConfig = NBCountConfig(countViewShowType:.CircleButton,tintColor:UIColor.greenColor())
+        countView1.notifyUpdateStepValue = {(updateStepValue : Int) in
+            print("更改的数量为:\(updateStepValue)")
+        }
+        countView1.notifyUpdateCurrentValue = {(currentValue : Int) in
+            print("当前显示数值为:\(currentValue)")
+        }
+
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        countView.resignFirstResponder()
     }
 
     override func didReceiveMemoryWarning() {
